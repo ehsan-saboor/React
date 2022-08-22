705aef6981ac944669b0b80483a2d971090869cb
unknown <akkhanahsan66@gmail.com>
2022-08-22 15:15:19 +0500

    Bootstrap App

--- my-app-1/src/App.js
+++ my-app-1/src/App.js
@@ -1,37 +1,42 @@
+import React, { useState } from "react";
 import AllExpenses from "./Components/Expenses/AllExpenses";
 import NewExpenses from "./Components/NewExpenses/NewExpenses";
 
+let DummyexpenseArray = [
+    {
+        id: "e1",
+        date: new Date(2022, 6, 10),
+        title: "School Fee",
+        amount: 300
+    },
+    {
+        id: "e2",
+        date: new Date(2022, 9, 11),
+        title: "School Fee",
+        amount: 250
+    },
+    {
+        id: "e3",
+        date: new Date(2022, 7, 12),
+        title: "School Fee",
+        amount: 300
+    },
+    {
+        id: "e4",
+        date: new Date(2022, 8, 13),
+        title: "School Fee",
+        amount: 200
+    }
+];
+
 const App = () => {
 
-    let expenseArray = [
-        {
-            id: "e1",
-            date: new Date(2022, 6, 10),
-            title: "School Fee",
-            amount: 300
-        },
-        {
-            id: "e2",
-            date: new Date(2022, 9, 11),
-            title: "School Fee",
-            amount: 250
-        },
-        {
-            id: "e3",
-            date: new Date(2022, 7, 12),
-            title: "School Fee",
-            amount: 300
-        },
-        {
-            id: "e4",
-            date: new Date(2022, 8, 13),
-            title: "School Fee",
-            amount: 200
-        }
-    ];
+    const [expenseArray, setexpenseArray] = useState(DummyexpenseArray)
 
     const againCollectFormData = (againEnteredExpenseData) =>{
-        console.log(againEnteredExpenseData)
+        const newaddedarray = [againEnteredExpenseData, ...expenseArray]
+        setexpenseArray(newaddedarray)
+
     }
 
     return (
