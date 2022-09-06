342535896d60cb4e045a039a2e5c14c9dc2bdaab
unknown <akkhanahsan66@gmail.com>
2022-09-06 06:07:30 +0500

    Updated 9/6/2022

--- first-react-website/src/components/about/About.jsx
+++ first-react-website/src/components/about/About.jsx
@@ -1,9 +1,53 @@
 import React from 'react'
 import "./about.css"
+import Me from "../../assets/me-about.jpg"
+import {FaAward} from "react-icons/fa"
+import {HiOutlineUsers} from "react-icons/hi"
+import {BsFillFolderSymlinkFill} from "react-icons/bs"
 
 const About = () => {
   return (
-    <div>About</div>
+    <section id='about'>
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={Me} alt="About Image" />
+          </div>
+        </div>
+
+        <div className="about__content">
+          <div className="about__cards">
+            
+            <article className="about__card">
+              <FaAward className='about__icon'/>
+              <h5>Experience</h5>
+              <small>3 + Experience</small>
+            </article>
+
+            <article className="about__card">
+              <HiOutlineUsers className='about__icon'/>
+              <h5>Clients</h5>
+              <small>200 + Worldwide</small>
+            </article>
+
+            <article className="about__card">
+              <BsFillFolderSymlinkFill className='about__icon'/>
+              <h5>Experience</h5>
+              <small>80 + Completed</small>
+            </article>
+            
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat, itaque numquam qui ipsum quas provident magnam et harum est minus quidem ad aliquam aspernatur excepturi in id? Soluta praesentium recusandae maxime perferendis atque consectetur deleniti, debitis aliquam perspiciatis neque accusamus reiciendis quasi voluptatum voluptatibus quia incidunt, repellat dolor maiores est delectus consequatur voluptatem esse! Fugiat voluptatum ex est sed facilis, minus quidem vitae cumque, aspernatur nihil ab accusamus cum voluptates quod adipisci magnam, quos illum fuga. Corporis et ut quaerat delectus sed beatae ad minus mollitia consectetur dolor doloremque, minima a repellendus? Consequatur veritatis nisi ex asperiores, quidem placeat pariatur.</p>
+
+            <a href="#contact" className='btn btn-primary'>Lets Talk</a>
+
+          </div>
+        </div>
+
+      </div>
+    </section>
   )
 }
 
--- first-react-website/src/components/header/Header.jsx
+++ first-react-website/src/components/header/Header.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
 import "./header.css"
+import CTA from "./CTA"
+import Me from "../../assets/me.png"
+import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header__container">
+        <h5>Hello This is</h5>
+        <h1>Some One</h1>
+        <h5 className='text-light'>Frontend Developer</h5>
+        <CTA />
+        <HeaderSocials />
+
+        <div className="me">
+          <img src={Me} alt="me" />
+        </div>
+
+        <a href="#contact" className='scroll__down'>Scroll Down</a>
+
+      </div>
+    </header>
   )
 }
 
