95066feee79655b8b60e9d46e0190f43b89d5d79
unknown <akkhanahsan66@gmail.com>
2022-09-08 03:27:34 +0500

    Complete React Website

--- first-react-website/src/components/contact/Contact.jsx
+++ first-react-website/src/components/contact/Contact.jsx
@@ -2,8 +2,25 @@ import React from 'react'
 import "./contact.css"
 import { MdEmail } from "react-icons/md"
 import { BsWhatsapp, BsMessenger } from "react-icons/bs"
+import { useRef } from 'react'
+import emailjs from "emailjs-com"
 
 const Contact = () => {
+
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_qutxndk', 'template_dcrqaed', form.current, 'z3zOG0BRyt0VCAM2k')
+      .then((result) => {
+        console.log(result.text);
+      }, (error) => {
+        console.log(error.text);
+      });
+    e.target.reset()
+  };
+
   return (
     <section id='contact'>
       <h5>Get In Touch</h5>
@@ -34,7 +51,7 @@ const Contact = () => {
           </article>
 
         </div>
-        <form action="">
+        <form ref={form} onSubmit={sendEmail}>
           <input type="text" name="name" placeholder='Your Full Name' required />
           <input type="email" name="email" placeholder='Your Email' required />
           <textarea name="message" rows="7" placeholder='Your Message' required></textarea>
